96b062e379c07245d0a329598acb1b5a99704718
ivonamojsovska <ivonamojsovska15@gmail.com>
2023-03-28 09:56:14 -0700

    localhost switched with 127.0.0.1

--- pages/[id].js
+++ pages/[id].js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 
 export const getStaticPaths = async () => {
-    const res = axios.get('http://localhost:8000/moviepicker').then(response => {
+    const data = await axios.get('http://127.0.0.1:8000/moviepicker').then((response) => {
         return response.data
-    })
+    }).catch(err => console.log(err))
 
-    const data = await res.json()
+    console.log(data)
 
-    const paths = data.map(movie => {
+
+    const paths = data.map((movie) => {
         return {
             params: { id: movie.id.toString() }
         }
@@ -19,28 +20,24 @@ export const getStaticPaths = async () => {
     }
 }
 
+export const getStaticProps = async (context) => {
+    const id = context.params.id
+    const res = await fetch(`http://127.0.0.1:8000/moviepicker/${id}`)
+    const data = await res.json()
 
+    return {
+        props: { movie: data }
+    }
 
+}
 
-// export const getStaticProps = async (context) => {
-//     const id = context.params.id
-//     const res = await fetch(`http://localhost:8000/moviepicker/${id}`)
-//     const data = await res.json()
+const Detils = ({ movie }) => {
 
-//     return {
-//         props: { movie: data }
-//     }
-
-// }
-
-
-const Detils = () => {
-
-    return (<div>
-        <h1>Movie</h1>
-
-
-    </div>);
+    return (
+        <div>
+            <h1>Movie</h1>
+            <h3>{movie.title}</h3>
+        </div>);
 }
 
 export default Detils;
